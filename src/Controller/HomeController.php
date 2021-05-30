15d78ed9ce3ed29ca04487c45cf7c18407d6d6c2
Achouri Bilel <ach.bilel1919@gmail.com>
2021-05-30 05:28:18 +0100

    panier 50%

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PlatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +19,16 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/commande", name="commande_index", methods={"GET"})
+     */
+    public function indexhome(PlatRepository $platRepository): Response
+    {
+        return $this->render('commande/index.html.twig', [
+            'plats' => $platRepository->findAll(),
         ]);
     }
 }
